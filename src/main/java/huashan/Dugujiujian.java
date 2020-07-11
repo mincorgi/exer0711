9199ae55cca4fe1b74b5a3ea2e4c7809cdae017c
mincorgi <mincorgi@163.com>
2020-07-11 13:51:06 +0800

    exer0711第一份

---
+++ src/main/java/huashan/Dugujiujian.java
@@ -0,0 +1,13 @@
+package huashan;
+
+/**
+ * @author Corgi
+ * @create 2020-07-10 15:52
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        //红色：未追踪代码     绿色：未追踪代码首次进入暂存区
+        //黑色：已提交本地库    蓝色：已提交本地库后又发生了修改且未提交
+    }
+}
